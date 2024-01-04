f075850721679f2e3f192a35add2ea20f7ab0346
Freyzan2006 <122388366+Freyzan2006@users.noreply.github.com>
2024-01-04 22:14:50 +0500

    Add files via upload

---
+++ first_project/blog/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+
+from .views import *
+
+urlpatterns = [
+    # path('', index, name='index'),
+    path('', ArticleList.as_view(), name='index'),
+    # path('category/<int:category_id>/', articles_by_category, name='category'),
+    path('category/<int:category_id>/', ArticlesByCategory.as_view(), name='category'),
+    # path('article/<int:article_id>/', article_detail, name='article_detail'),
+    path('article/<int:pk>/', ArticleDetail.as_view(), name='article_detail'),
+    path('add_article/', AddArticle.as_view(), name='add_article'),
+    path('profile/', profile, name='profile'),
+    path('login/', user_login, name='login'),
+    path('logout/', user_logout, name='logout'),
+    path('register/', register, name='register'),
+    path('add_comment/<int:article_id>', save_comment, name='add_comment'),
+
+]
+
